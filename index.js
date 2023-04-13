09750d61b274f5961908eff62c1eb6813b986d03
Jacob Smith <jacobwillsmith@gmail.com>
2023-04-13 01:45:36 -0700

    fix

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import express from 'express';
 import path from 'path';
+import * as url from 'url';
 const app = express();
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
-app.use(express.static(path.join(new URL(import.meta.url).pathname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
