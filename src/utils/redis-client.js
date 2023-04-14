2c1f46194cadd92acd0bbd8e8d3656a8b499eb16
Jacob Smith <jacobwillsmith@gmail.com>
2023-04-14 12:12:27 -0700

    log

--- src/utils/redis-client.js
+++ src/utils/redis-client.js
@@ -1,5 +1,10 @@
 import redis from 'redis';
 
+const debug = false;
+const log = (str) => {
+  if (debug) console.log(str);
+}
+
 class RedisPool {
   constructor({ url, maxConnections = 20 }) {
     this.url = url;
@@ -10,25 +15,25 @@ class RedisPool {
   }
 
   async acquire() {
-    console.log('Acquiring client from pool (pool size: ' + this.pool.length + ')');
+    log('Acquiring client from pool (pool size: ' + this.pool.length + ')');
     if (this.pool.length < this.maxConnections) {
       const client = redis.createClient({ url: this.url });
       this.pool.push(client);
-      console.log(`New connection added to pool (pool size: ${this.pool.length})`);
+      log(`New connection added to pool (pool size: ${this.pool.length})`);
     }
 
     const client = this.pool.pop();
     if (client)
     {
       await client.connect();
-      console.log('Acquired client from pool (pool size: ' + this.pool.length + ')');
+      log('Acquired client from pool (pool size: ' + this.pool.length + ')');
       this.connections.push(client);
-      console.log(`New connection added to connections (connections size: ${this.connections.length})`);
+      log(`New connection added to connections (connections size: ${this.connections.length})`);
       return client;
     }
     else
     {
-      console.log(`No available connections, waiting...`);
+      log(`No available connections, waiting...`);
       return new Promise((resolve) => {
         this.waiting.push(resolve);
       }).then(() => this.acquire());
@@ -40,10 +45,10 @@ class RedisPool {
     const index = this.connections.indexOf(client);
     if (index !== -1) {
       this.connections.splice(index, 1);
-      console.log(`Connection removed from connections (connections size: ${this.connections.length})`);
+      log(`Connection removed from connections (connections size: ${this.connections.length})`);
     }
     this.pool.push(client);
-    console.log(`Connection returned to pool (pool size: ${this.pool.length})`);
+    log(`Connection returned to pool (pool size: ${this.pool.length})`);
     if (this.waiting.length > 0) {
       const resolve = this.waiting.shift();
       resolve(this.acquire());
@@ -71,13 +76,9 @@ const saveAlbumData = async (albumId, data) => {
 
 // Retrieve album data by album ID
 const getAlbumData = async (albumId) => {
-  console.log('asking for album: ' + albumId);
   const client = await getClient();
-  console.log('have client');
   const data = await client.get(`album:${albumId}`);
-  console.log('have data');
   releaseClient(client);
-  console.log('release');
   return data ? JSON.parse(data) : data;
 };
 
