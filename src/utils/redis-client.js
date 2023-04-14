2814d0dff337b2db8fc1f1ab3c64f67f6da1d079
Jacob Smith <jacobwillsmith@gmail.com>
2023-04-14 00:22:16 -0700

    fix

--- src/utils/redis-client.js
+++ src/utils/redis-client.js
@@ -1,26 +1,35 @@
 import redis from 'redis';
 
-const client = redis.createClient(process.env.REDIS_URL);
+let client;
 
-client.on('connect', () => {
-  console.log('Redis client connected');
-});
+const connect = () => {
+  client = redis.createClient(process.env.REDIS_URL);
 
-client.on('error', (err) => {
-  console.error('Redis error:', err);
-});
+  client.on('connect', () => {
+    console.log('Redis client connected');
+  });
 
-client.on('end', () => {
-  console.log('Redis client disconnected');
-});
+  client.on('error', (err) => {
+    console.error('Redis error:', err);
+  });
+
+  client.on('end', () => {
+    console.log('Redis connection closed');
+    setTimeout(connect, 5000);
+  });
+};
+
+connect();
 
 // Save album data by album ID
 const saveAlbumData = (albumId, data) => {
+  if (!client.connected) connect();
   client.set(`album:${albumId}`, JSON.stringify(data));
 };
 
 // Retrieve album data by album ID
 const getAlbumData = (albumId, callback) => {
+  if (!client.connected) connect();
   client.get(`album:${albumId}`, (err, data) => {
     if (err) throw err;
     callback(JSON.parse(data));
@@ -29,11 +38,13 @@ const getAlbumData = (albumId, callback) => {
 
 // Save track data by track ID
 const saveTrackData = (trackId, data) => {
+  if (!client.connected) connect();
   client.set(`track:${trackId}`, JSON.stringify(data));
 };
 
 // Retrieve track data by track ID
 const getTrackData = (trackId, callback) => {
+  if (!client.connected) connect();
   client.get(`track:${trackId}`, (err, data) => {
     if (err) throw err;
     callback(JSON.parse(data));
