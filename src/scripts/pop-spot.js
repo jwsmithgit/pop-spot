f7c0936583965157d96fac89999839340dc3bf38
Jacob Smith <jacobwillsmith@gmail.com>
2023-04-17 00:49:38 -0700

    fix

--- src/scripts/pop-spot.js
+++ src/scripts/pop-spot.js
@@ -404,7 +404,7 @@ export async function execute(accessToken) {
     let artistAlbums = await getArtistAlbums(accessToken, Object.values(artists).map(artist => artist.id));
     albums = await getAlbums(accessToken, Object.values(artistAlbums).flat());
     tracks = await getTracks(accessToken, Object.values(albums).flatMap(album => album.trackIds));
-    let albumTracks = tracks.reduce((acc, track) => ({...acc, [track.id]: track}), {});
+    let albumTracks = Object.values(tracks).reduce((acc, track) => ({...acc, [track.id]: track}), {});
     
     let albumPopTracks = Object.values(albumTracks).map(tracks => getPopularTracks(tracks));
     let popTracks = Object.values(albumPopTracks).flat();
